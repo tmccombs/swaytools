4dbd656359a90f7acd3dcbe9992738669464d1a3
Thayne McCombs <astrothayne@gmail.com>
2020-02-06 01:27:53 -0700

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,20 @@ setup(
         packages=find_packages(),
         license='GPL3',
         author='Thayne McCombs',
+        url='https://github.com/tmccombs/swaytools',
+        keywords='sway i3 tools',
         python_requires='>=3.8.0',
         entry_points={
             'console_scripts': [
                 'swayinfo = swaytools.info:main',
                 'winfocus = swaytools.winfocus:main',
             ]
-        }
+        },
+        classifiers=[
+            "Environment :: Wayland Applications :: sway",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3",
+            "Topic :: Desktop Environment",
+        ]
 )
