decfba4ad5c286f6e7a6c8f95d3760a5a8fb20bc
Thayne McCombs <astrothayne@gmail.com>
2020-02-06 22:43:43 -0700

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
         name='swaytools',
         version='0.1.0',
         description='Quality of life tools for sway',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         packages=find_packages(),
         license='GPL3',
         author='Thayne McCombs',
